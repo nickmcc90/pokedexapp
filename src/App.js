deda348b0beca5d07a057d2c927c49e3388db844
Nicholas McCarroll <nicholasmccarroll@Nicholass-MacBook-Air.local>
2024-03-31 18:08:26 -0400

    added header and image up top

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './styles/App.css'
+import masterball from './icons/game.png'
 
 function App() {
   return (
     <body>
-      <h1>Pokedex Pokemon Viewer</h1>
+      <h1 className="header">Pokedex Pokemon Viewer</h1>
+      <div className="img-container">
+        <img className="masterball" src={masterball} />
+      </div>
     </body>
   );
 }
