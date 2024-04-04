572fb1c340e532396f85d6ea4e3653877c2caade
Nicholas <nickmccarroll8@gmail.com>
2024-04-03 20:17:56 -0400

    finally figured out API calls

--- src/App.js
+++ src/App.js
@@ -24,3 +24,45 @@ export default App;
 // We gotta add the poke API to our firebase... then grab it from the database. Next time 4/1/24
 
 // figure out why we cant call from a second api link.... 4/2/24
+
+// what i did is make another handleresponse function to call once we have a nested url
+
+// YYOOOOOOO i solved it!!! Once you have your first urls in a html element from mapping the results,
+// it'll be trapped within an object. You need to get the data from this object by console.log-ing the
+// object, then figuring out what dot notation you need to take. Check APIcall.jsx for this knowledge!!!
+
+
+// We can have multiple arguments in a .then call if a previous .then returns an object. We can use the keys
+// within this previously returned object as arguments within curly brackets 
+// Ex:
+/* 
+
+myPromise.then(a => {
+    var b = 122;
+    return {
+        a,
+        b,
+        c: 'foo'
+    };
+}).then(({ a, b, c }) => {
+    console.log(a);
+    console.log(b);
+    console.log(c);    
+});
+
+*/
+
+// 1. Optimize API call to not use all the handleResponse code, have some test cases to handle things accordingly. We
+// can do this in part by being able to pass multiple arguments into the .then when we call the handleResponse function.
+// 2. Store images an array just like we can do the names.
+// 3. Dont forget that we need to load more, so we need to reuse these handleResponse to add more to the array, or maybe
+// make a new array.
+
+
+
+
+
+// WE GOT THE SPRITE!!!!!
+
+// Task for tomorrow.... figure out how to have an array of front sprites and shiny front sprites.
+// Do the front sprites first.
--- src/components/APIcall.jsx
+++ src/components/APIcall.jsx
@@ -11,24 +11,61 @@ export default function APIcall() {
       return response.json();
     }
 
+    const handleResponse3 = (response) => {
+      const frontImgPath = response.sprites.front_default;
+      const image = <img src={frontImgPath} />;
+
+      setPokemon(image)
+    }
+
+    const handleResponse2 = (response) => {
+      const evenDeeperUrl = response.forms[0].url;
+      console.log(evenDeeperUrl);
+
+      fetch(evenDeeperUrl)
+      .then(handleFetch)
+      .then(handleResponse3)
+      .catch(handleError)
+
+    }
+
     const handleResponse = (response) => {
-      //names
+      //names. we save the names into an array too.
       const respPoke = response.results.map((item) => <li>{item.name}</li>);
+      const namesArray = [];
+      respPoke.forEach((pokepoke) => {
+        namesArray.push(pokepoke.props.children);
+      })
+      console.log(namesArray);
 
       //pictures
       const pokeUrl = response.results.map((item) => <li>{item.url}</li>);
 
-      let frontImgPaths;
+      /*
+        Here, pokeUrl is an html element with all the urls inside. But they aren't just urls, they are objects
+        with different keys, and the urls lie within the key 'children'. We need to access this 'children' key
+        and put them into an array. With this, we can now put each link into another handleResponse function to
+        get its data.
+      */
 
-      fetch(pokeUrl)
-      .then((response) => {frontImgPaths = response.json().map((item) => <li>{item.base_experience}</li>)})
-      .catch(setPokemon(<li>Help!</li>))
+      const deeperUrls = []
+
+      pokeUrl.forEach((urlThing) => {
+        deeperUrls.push(urlThing.props.children);
+      })
+      console.log(deeperUrls[0]);
+
+
+      fetch(deeperUrls[0])
+      .then(handleFetch)
+      .then(handleResponse2)
+      .catch(handleError)
+
 
       // const frontImgPaths = pokeUrl.map((item) => <li>{item.base_experience}</li>)
 
       // frontImgPaths = <li>{url.forms.sprites.front_default}</li>;
 
-      setPokemon(frontImgPaths);
       //this allows other components to call what they need from the API
       //we should add things to the database here
     }
